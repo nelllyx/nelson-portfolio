8d6faf754b162509caf53be6be09464e1319af52
nelllyx <nelsonakewe0@gmail.com>
2025-07-25 15:46:48 +0100

    fixes

--- src/component/portfolio/Projects.jsx
+++ src/component/portfolio/Projects.jsx
@@ -9,12 +9,16 @@ const Projects = () => {
     useEffect(() => {
         if (isOpen) {
             document.body.style.overflow = 'hidden';
+            document.body.style.position = 'fixed';
+            document.body.style.width = '100%';
         } else {
             document.body.style.overflow = 'auto';
+            document.body.style.position = 'static';
         }
 
         return () => {
             document.body.style.overflow = "auto"
+            document.body.style.position = 'static';
         }
 
     }, [isOpen]);
